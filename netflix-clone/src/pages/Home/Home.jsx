2b6801bb270c45919ec6405e1fd75cbcea80b6e9
Solomon-Amanuel <solomonamanuel2018@gmail.com>
2024-11-26 04:12:51 +0100

    rows added

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner';
+import RowLists from '../../components/Rows/RowLists/RowLists'
 
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Banner/>
+      <Banner />
+      <RowLists />
       <Footer />
     </>
   );
